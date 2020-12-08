9a5248ed297afc40b891f14f862860c928fce649
Gabrieledigennaro <74955914+Gabrieledigennaro@users.noreply.github.com>
2020-12-08 17:19:10 +0100

    Update Grafico2.py

--- Grafico2.py
+++ Grafico2.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 
-data = {'leoni': 40, 'gazzelle': 55, 'zebre': 108, 'iene': 40}
+data = {'calcio': 130, 'pallavolo': 75, 'basket': 50, 'nuoto': 40}
 names = list(data.keys())
 values = list(data.values())
 
 fig, axs = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
 axs[0].bar(names, values)
 axs[1].scot(names, values)
-fig.suptitle('Animali presenti in una area protetta')
+fig.suptitle('studenti che praticano sport')
 atter(names, values)
 axs[2].pl
 
