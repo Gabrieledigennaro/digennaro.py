09a99975eab3cb71520a295075238fe027a8c756
Gabrieledigennaro <digennaro.gabriele.s19@liceoviconapoli.it>
2021-03-02 16:20:13 +0100

    Create librerie.py

---
+++ json.py/librerie.py
@@ -0,0 +1,66 @@
+import json
+
+librerie_STRINGA = ' ' '
+{ 
+    "people" : [
+        {  
+            "Nome": "L'ombra del vento",
+            "autore" : " Carlos Ruis Zafon"
+            "genere" : " romanzo thriller"
+            "casa editrice" :" Mondadori"
+        },
+        {  
+            "Nome": " La spada di Shannara"
+            "autore" : " Terry Brooks"
+            "genere" : " romanzo fantastico"
+            "casa editrice" : "Mondadori"
+        },
+        {  
+            "Nome": " Il dio del fiume"
+            "autore" : " Wilbur Smith"
+            "genere" :  " romanzo d'avventura"
+            "casa editrice" : " BestTEA "
+        },
+        {  
+            "Nome": " Harry Potter"
+            "autore" : " J.K. Rowling"
+            "genere" :  "romanzo fantasy"
+            "casa editrice" : " Salani editore"
+        },
+        {  
+            "Nome": " Cronache marziane"
+            "autore" : " Ray Bradbury"
+            "genere" :  "racconto fantascientifico"
+            "casa editrice" : " Mondadori"
+        },
+        {  
+            "Nome": "La città perduta del dio scimmia"
+            "autore" : " Douglas Preston"
+            "genere" : "romanzo d'avventura reale"
+            "casa editrice" : " Rizzoli"
+        },
+        {  
+            "Nome": " Il grande mondo laggiù"
+            "autore" : " Ray Brudbury"
+            "genere" : " racconto fantascientifico"
+            "casa editrice" : "Mondadori"
+        },
+        {  
+            "Nome": " Il nome della rosa"
+            "autore" : " Umberto Eco"
+            "genere" :  "romanzo storico"
+            "casa editrice" : "Bombiani"
+        },
+        {  
+            "Nome": " I pilastri della terra"
+            "autore" : "Ken Follet"
+            "genere" :  " romanzo storico "
+            "casa editrice" : " Mondadori"
+        }, 
+        {  
+            "Nome": "Diario di una schiappa"
+            "autore" : " Jeff Kinney"
+            "genere" :  " Commedia"
+            "casa editrice" : "Abram books"
+        },
+
