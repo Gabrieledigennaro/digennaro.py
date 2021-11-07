3528e914eefadd2e45a8a60f8c83d7ce7a3a6146
Gabrieledigennaro <74955914+Gabrieledigennaro@users.noreply.github.com>
2021-11-07 22:26:41 +0100

    Update retta2.py

--- oop.py/coniche/retta2.py
+++ oop.py/coniche/retta2.py
@@ -1,41 +1,75 @@
+import math
+print("Lavorare con equazione scritta nella forma: ax + by + c = 0")
 class retta:
     
 
-    def _init_(self, a, b, c):
+    def __init__(self, a, b, c):
         self.__a = float(a)
         self.__b = float(b)
         self.__c = float(c)
         self.__punti = []
 
+    def getA(self):
+        return f"\n a = {self.__a}"
+    
+    def getB(self):
+        return f"\n b = {self.__b}"
+
+    def getC(self):
+        return f"\n c = {self.__c}"
+
+    def a_b_c_da_punti(self, x1, x2, y1, y2):
+        self.__x1 = int(x1)
+        self.__x2 = int(x2)
+        self.__y1 = int(y1)
+        self.__y2 = int(y2)
+        x_d = (self.__x2 - self.__x1)
+        y_d = (self.__y2 - self.__y1)
+        MCD = math.gcd(x_d, y_d)
+        mcm = (x_d * y_d) / MCD
+        a = mcm / x_d
+        b = mcm / y_d
+        c = (mcm / x_d * -self.__x2) + (mcm / y_d * self.__y2)
+        print("a = ", a,',', "b = ", b,',', "c = ",c)
+
+    def a_b_c_da_m(self, x3, y3, m1):
+        self.__x3 = int(x3)
+        self.__y3 = int(y3)
+        self.__m1 = int(m1)
+        a = self.__m1
+        b = -1
+        c = (self.__m1 * -self.__x3)+self.__y3
+        print("a = ", a,',', "b = ", b,',', "c = ",c)
+
 
     def Implicita(self):
         if self.__b == 0:
-            return f"\nForma implicita dell'equazione:\n {self._a}x + {self._c} = 0"       
+            return f"\nForma implicita dell'equazione:\n {self.__a}x + {self.__c} = 0"       
         elif self.__a == 0:
-            return f"\nForma implicita dell'equazione:\n {self._b}y + {self._c} = 0"    
+            return f"\nForma implicita dell'equazione:\n {self.__b}y + {self.__c} = 0"    
         elif self.__c == 0:
-            return f"\nForma implicita dell'equazione:\n {self._a}x + {self._b}y = 0"    
+            return f"\nForma implicita dell'equazione:\n {self.a}x + {self.__b}y = 0"    
         else:   
-            return f"\nForma implicita dell'equazione:\n {self._a}x + {self.b}y + {self._c} = 0 "
+            return f"\nForma implicita dell'equazione:\n {self.__a}x + {self.__b}y + {self.__c} = 0 "
 
     def Esplicita(self):
         if self.__b == 0:
             return f"\nForma esplicita dell'equazione: \n L'equazione è impossibile"
         elif self.__a == 0:
-            return f"\nForma esplicita dell'equazione: \n y = {-self._c / self._b}"
+            return f"\nForma esplicita dell'equazione: \n y = {-self.__c / self.__b}"
         elif self.__c == 0:
-            return f"\nForma esplicita dell'equazione: \n y = {-self._a / self._b}"
+            return f"\nForma esplicita dell'equazione: \n y = {-self.__a / self.__b}"
         else:
-            return f"\nForma esplicita dell'equazione: \n y = {-self._a / self.b}x + {-self.c / self._b}"
+            return f"\nForma esplicita dell'equazione: \n y = {-self.__a / self.__b}x + {-self.__c / self.__b}"
     
     def m(self):
         if self.__b == 0:
             return f"\nCoefficiente angolare: \n Il coefficiente angolare non è definito; la retta è parallela all'asse y"
         else:
-            return f"\nCoefficiente angolare: \n m = {-self._b / self._a}"
+            return f"\nCoefficiente angolare: \n m = {-self.__b / self.__a}"
     
     def trovaY(self, x):
-        return f"\n Y: \n y = {-self._a * x / self.b + (-self.c / self._b)}"
+        return f"\n Y: \n y = {-self.__a * x / self.__b + (-self.__c / self.__b)}"
 
 
     def punti(self, N, M, x):
@@ -43,7 +77,7 @@ class retta:
         self.M = M
     
         for self.N in range (self.M):
-            tupla = (x, (-self._a * x) / self.b + (-self.c / self._b))
+            tupla = (x, (-self.__a * x) / self.__b + (-self.__c / self.__b))
             x = x + 1
             self.__punti.append(tupla)
         return f"\n Le coordinate dei punti appartenenti alla retta sono: \n {self.__punti}"         
@@ -53,20 +87,30 @@ class retta:
         self.__a1 = float(a1)
         self.__b1 = float(b1)
         self.__c1 = float(c1)
-        if (-self._b / self.a) == (-self.b1 / self._a1):
-            if self._c == self._c1:
+        if (-self.__b / self.__a) == (-self.__b1 / self.__a1):
+            if self.__c == self.__c1:
                 return f"\nLe rette sono coincidenti \n {self.__punti}"
             else:
                 return f"Null"
-        elif self._c == self._c1:
-            return f"\nIl putnto di incontro delle due rette è: {self.__c}y" 
+        elif self.__c == self.__c1:
+            return f"\nIl putnto di incontro delle due rette è: (0, {self.__c})" 
         else:
-            return f"\nLe rette sono incidenti e le coordinate del punto d'incidenza sono: ({((-self._c / self.b)+(self.c1 / self.b1))/((-self.b / self.a)+(self.b1 / self.a1))}, {((-self.b / self.c)+(self.b1 / self.c1))/((-self.b / self.a)+(self.b1 / self._a1))})"
+            return f"\nLe rette sono incidenti e la coordinata del punto d'incidenza è: ({((-self.__c / self.__b)+(self.__c1 / self.__b1))/((-self.__b / self.__a)+(self.__b1 / self.__a1))}, {((-self.__b / self.__c)+(self.__b1 / self.__c1))/((-self.__b / self.__a)+(self.__b1 / self.__a1))})"
 
-valori = retta(2, 3, 4)
+    def fascio_parallelo(self):
+        if self.__b == 0:
+            return f"\nL'equazione del fascio parallelo alla retta data è: \nx = k"
+        else:
+            return f"\nL'equazione del fascio parallelo alla retta data è: \ny = {-self.__a / self.__b}x + q"
+
+valori = retta(input('a = ' ), input('b = ' ), input('c = ' ))
 print(valori.Implicita())
 print(valori.Esplicita())
 print(valori.m())
-print(valori.trovaY(2))
-print(valori.punti(0, 600, -300))
-print(valori.instersezione(1, 2, 5))
+print(valori.trovaY(input('x = ')))
+print(valori.punti(input('inizio intervallo = ') , input('fine intervallo = ')))
+print(valori.instersezione(input('a1 = ' ), input('b1 = ' ), input('c1 = ' )))
+print(valori.fascio_parallelo())
+print(valori.a_b_c_da_punti(input('x1 = ' ), input('x2 = ' ), input('y1 = ' ), input('y2 = ' )))
+print(valori.a_b_c_da_m(input('x1 = ' ), input('y1 = ' ), input('m = ' )))
+ 
